3b8c50359edca0574c641689d4d4a169f6bf3a95
p59740 <121592993+p59740@users.noreply.github.com>
2023-12-04 20:06:18 -0800

    a6 test

--- src/Project/users/client.js
+++ src/Project/users/client.js
@@ -6,6 +6,7 @@ const request = axios.create({  // create a configured instance of axios
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 // export const USERS_API = `${BASE_API}/api/users`;
 export const USERS_API = 'https://kanbas-node-db-server-app.onrender.com/api/users'
+
 export const signin = async (credentials) => {
   const response = await request.post( `${USERS_API}/signin`, credentials );
   return response.data;
